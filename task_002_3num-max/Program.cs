050f2612f142cdf843a91c3d31475e5491e0f7b5
Дмитрий Копылов <dkopylov@MacBook-Air-2.local>
2022-08-22 17:26:27 +0300

    task2 was added

---
+++ task_002_3num-max/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Please, enter num1 ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please, enter num2 ");
+int numB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please, enter num3 ");
+int numC = int.Parse(Console.ReadLine());
+
+int max = numA;
+
+if (numB > max)
+{
+    max = numB;
+}
+
+if (numC > max)
+{
+    max = numC;
+}
+
+Console.WriteLine("Maximum is - " + max);
