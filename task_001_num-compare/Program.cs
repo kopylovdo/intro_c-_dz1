afd3d13be408f9427a10d126a64e71bfa2737bd0
Дмитрий Копылов <dkopylov@MacBook-Air-2.local>
2022-08-22 16:57:50 +0300

    task1 was added

---
+++ task_001_num-compare/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Please, enter num1 ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please, enter num2 ");
+int numB = int.Parse(Console.ReadLine());
+
+if (numA > numB)
+{
+    Console.WriteLine(numA + " larger than " + numB);
+}
+else
+{
+    Console.WriteLine(numB + " larger than " + numA);
+}
