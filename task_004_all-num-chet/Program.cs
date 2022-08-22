20d9df057d6cccaaf30d92aac0cb3d6e9c73db17
Дмитрий Копылов <dkopylov@MacBook-Air-2.local>
2022-08-22 18:30:40 +0300

    task4 was added

---
+++ task_004_all-num-chet/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Please, enter N ");
+int N = int.Parse(Console.ReadLine());
+
+int index = 1;
+
+while (index <= N)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write(index + ", ");
+    }
+    index++;
+}
+Console.WriteLine();
