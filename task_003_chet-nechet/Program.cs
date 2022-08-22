2abafe5164c956a915319f88271f5197b818ee7c
Дмитрий Копылов <dkopylov@MacBook-Air-2.local>
2022-08-22 17:40:39 +0300

    task3 was added

---
+++ task_003_chet-nechet/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Please, enter number ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine(number + " chetnoe chislo");
+}
+else
+{
+    Console.WriteLine(number + " nechetnoe chislo");
+}
